38d6ea00f1a5afe17e02d1bd2971b502b6cf0ffc
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2025-03-27 02:09:14 -0300

    build: adding native wind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
 import { fonts } from "@/constants/fonts"
 import { Text, View } from "react-native"
+import "../global.css"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 justify-center items-center">
       <Text style={{ fontFamily: fonts.family.regular }}>
         Edit app/index.tsx to edit this screen.
       </Text>
