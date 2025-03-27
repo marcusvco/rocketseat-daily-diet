1f891ec33982fa3fa18e95d137ae0049dc408457
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2025-03-27 01:31:01 -0300

    build: adding nunito sans font and phosphor icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,31 @@
-import { Stack } from "expo-router";
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from "@expo-google-fonts/nunito-sans"
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native"
+import { SplashScreen, Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
+import { useEffect } from "react"
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  })
+
+  useEffect(() => {
+    if (loaded || error) SplashScreen.hideAsync()
+  }, [loaded, error])
+
+  if (!loaded && !error) return null
+
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </ThemeProvider>
+  )
 }
